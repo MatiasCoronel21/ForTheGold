efa0dab9ab1dab71339d483a9117d74e2a1cd8ea
MatiasCoronel21 <62911294+MatiasCoronel21@users.noreply.github.com>
2020-09-28 12:30:31 -0300

    Cambio Leve de las posiciones de niveles
    modificación para que tengra mejor dinamica dentro de la jugabilidad

---
+++ ForTheGold/Assets/Scripts/CambioSprite.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CambioSprite : MonoBehaviour {
+
+    public Sprite spriteInvicibilidad;
+
+    public SpriteRenderer personajeCambiable;
+
+
+
+    public void Invencibilidad()
+    {
+
+        personajeCambiable.sprite = spriteInvicibilidad;
+    }
+}
